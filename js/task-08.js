214a6132bf98357fed32b31f692fe269b2327b69
Zhenya Horlach <gorlachjob@ukr.net>
2021-11-12 19:22:25 +0200

    Вносит правки в js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formSubmit = document.querySelector(".login-form");
 
+const onSubmitForm = (event) => {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return alert('Please fill in all the fields!');
+	}
+
+	console.log(`Email: ${email.value}, Password: ${password.value}`);
+	event.currentTarget.reset();
+};
+
+formSubmit.addEventListener("submit", onSubmitForm);
